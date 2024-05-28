4bdc02877e9809055fc005ccd58134a905f83dcf
Felicio <victor_felicio@hotmail.com>
2024-05-28 00:44:13 -0300

    feat: config errors and add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import { routers } from './routes';
 
 const server = express();
@@ -6,5 +7,18 @@ const server = express();
 server.use(express.json());
 
 server.use(routers);
+
+server.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            error: err.message,
+        });
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal Server Error',
+    });
+});
 
 server.listen(3333, () => console.log('Server list on port :3333!!'));
